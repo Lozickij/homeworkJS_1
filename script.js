367c9ff6e9f0bbf9368cd055fe635102a3ca7179
АЛЕКСАНДР ЛОЗИЦКИЙ <avtolik2015@outlook.com>
2023-02-02 22:36:17 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let tempC = 25;
+let tempF = (9 / 5) * tempC + 32;
+console.log(`Температура по цельсию - ${tempC}°;
+температура по фаренгейту - ${tempF}°.`);
+
+let firstName = 'Aleksandr';
+let admin = firstName;
+console.log(admin);
